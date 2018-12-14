6dd04ed242eee503a86255531f350bf894b5e376
joe <taojoe@gmail.com>
2018-12-14 15:26:45 +0800

    修改名称

--- jdbc/project.clj
+++ jdbc/project.clj
@@ -1,12 +1,12 @@
-(defproject ragtime/jdbc "0.7.2"
+(defproject com.github.taojoe/ragtime.jdbc "0.7.2"
   :description "Ragtime migrations for JDBC"
   :url "https://github.com/weavejester/ragtime"
   :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.jdbc "0.5.8"]
-                 [ragtime/core "0.7.2"]
+                 [com.github.taojoe/ragtime.core "0.7.2"]
                  [resauce "0.1.0"]]
   :profiles
   {:dev {:dependencies [[com.h2database/h2 "1.3.160"]]}})
